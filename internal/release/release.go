49635d33bee9fb117b4acc4a9e9f136d04dd392d
Uğur Özyılmazel <ugur.ozyilmazel@vbyazilim.com>
2024-12-26 19:25:44 +0300

    bump-my-version is not working properly

--- internal/release/release.go
+++ internal/release/release.go
@@ -4,4 +4,4 @@ package release
 const Version = "0.2.3"
 
 // BuildInformation holds current build information.
-var BuildInformation = "476a4fa806f62eeee5899c8d43d924510e354085"
+var BuildInformation = "91c5ab0c34c3"
