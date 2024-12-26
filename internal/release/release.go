dd481c09e71bae8ce3aa4ee2a70fc42deeb48c7f
Uğur Özyılmazel <ugur.ozyilmazel@vbyazilim.com>
2024-12-26 19:37:28 +0300

    Bump version: 0.3.0 → 0.3.1

--- internal/release/release.go
+++ internal/release/release.go
@@ -1,7 +1,7 @@
 package release
 
 // Version is the current version of service.
-const Version = "0.3.0"
+const Version = "0.3.1"
 
 // BuildInformation holds current build information.
 var BuildInformation = "d09aa2a272f6"
